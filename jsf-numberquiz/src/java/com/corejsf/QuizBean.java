b94ea88b1e22166a7370de2e0c69a05e84f618a9
mlhazan <mlhazan@hotmail.com>
2016-10-29 18:06:21 -0400

    Quiz result and page render

--- jsf-numberquiz/src/java/com/corejsf/QuizBean.java
+++ jsf-numberquiz/src/java/com/corejsf/QuizBean.java
@@ -1,4 +1,3 @@
-
 package com.corejsf;
 
 import java.io.Serializable;
@@ -8,35 +7,70 @@ import javax.faces.bean.SessionScoped;
 
 @ManagedBean
 @SessionScoped
-public class QuizBean implements Serializable{
-    private ArrayList<ProblemBean> problems = new ArrayList<ProblemBean>();
-    private int currentIndex, score;
-    
-    public QuizBean(){
-         
-         problems.add(new ProblemBean(new int[]{3,1,4,1,5},9));//pi
-         problems.add(new ProblemBean(new int[]{1,1,2,3,5},8));//fibonacci
-         problems.add(new ProblemBean(new int[]{1,4,9,16,25},36));//squares
-         problems.add(new ProblemBean(new int[]{2,3,5,7,11},13));//primes
-         problems.add(new ProblemBean(new int[]{1,2,4,8,16},32));//power of 2
-    }
-    
+public class QuizBean implements Serializable {
+
+     private ArrayList<ProblemBean> problems = new ArrayList<ProblemBean>();
+     private int currentIndex, score;
+     boolean valid = false;
+     String url;
+     int index = 0;
+
+     public QuizBean() {
+
+//          problems.add(new ProblemBean(new int[]{3, 1, 4, 1, 5}, 9));//pi
+//          problems.add(new ProblemBean(new int[]{1, 1, 2, 3, 5}, 8));//fibonacci
+          problems.add(new ProblemBean(new int[]{1, 4, 9, 16, 25}, 36));//squares
+//         problems.add(new ProblemBean(new int[]{2, 3, 5, 7, 11}, 13));//primes
+          problems.add(new ProblemBean(new int[]{1, 2, 4, 8, 16}, 32));//power of 2
+     }
+
 //    public void QuizBean(ArrayList<ProblemBean> newValue){//it was setProblems
 //         problems = newValue;
 //         currentIndex = 0;
 //         score = 0;
 //    }
-    public int getScore(){ return score;}
-    
-    public ProblemBean getCurrent(){ return problems.get(currentIndex);}
-   
-    public String getAnswer(){ return "";}
-    
-    public void setAnswer(String newValue){
-         try{
-              int answer = Integer.parseInt(newValue.trim());
-              if(getCurrent().getSolution() == answer) score ++;
-              currentIndex = (currentIndex +1)% problems.size();
-         }catch(NumberFormatException e){}
-    }
+     public int getScore() {
+          return score;
+     }
+
+     public ProblemBean getCurrent() {
+          if (index == 2) {
+               setValid("result.xhtml");
+          }
+          return problems.get(currentIndex);
+     }
+
+     public String getAnswer() {
+          return "";
+     }
+
+     public void setAnswer(String newValue) {
+          try {
+
+               if (index < problems.size()) {
+                    int answer = Integer.parseInt(newValue.trim());
+                    if (getCurrent().getSolution() == answer) {
+                         score++;
+                         index++;
+
+                    }
+
+                    currentIndex = (currentIndex + 1) % problems.size();
+               }
+          } catch (NumberFormatException e) {
+          }
+     }
+
+     public String getUrl() {
+          return url;
+     }
+
+     public void setValid(String url) {
+          this.url = url;
+          valid = true;
+     }
+
+     public boolean getValid() {
+          return valid;
+     }
 }
