a49c5d19fe24a730d3ad860cf1a5449c2ca052f2
mlhazan <mlhazan@hotmail.com>
2017-01-03 08:52:26 -0500

    Complete login app with db

---
+++ jsf-login-app/web/Resources/Credentials.sql
@@ -0,0 +1,3 @@
+CREATE TABLE Credentials (username varchar(20), password varchar(20), logincount integer);
+insert into credentials values ('hasan', 'safe',0);
+insert into credentials values ('mithun', 'face',0);
